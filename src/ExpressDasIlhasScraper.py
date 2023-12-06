fae4e1ca7236fee9fd118925423fcc777e6ab5ab
Eric Tavares <ericmartial.tavares@gmail.com>
2023-12-06 16:25:32 -0100

    Finished ExpressDasIlhasScraper. Export requirements.txt correctly

--- src/ExpressDasIlhasScraper.py
+++ src/ExpressDasIlhasScraper.py
@@ -1,15 +1,15 @@
 import asyncio
 import html as hypertext
+import re
 
 import dateparser
+from httpx import AsyncClient
 from parsel import Selector
 from rich.console import Console
 
 from .BaseScraper import BaseScraper
 from .StorageWorker import StorageWorker
 from .utils import normalize_date
-from icecream import ic
-import re
 
 SENTINEL = "STOP"
 
@@ -22,7 +22,7 @@ class ExpressDasIlhasScraper(BaseScraper):
             resp = await self.fetch_page(client, page_url)
             html = Selector(text=resp.text)
             urls = html.css(".featuredContent > a.intern::attr(href)").getall()
-            
+
             console.print(
                 f"Found [cyan]{len(urls)}[/cyan] URLs on page [blue]{page_url}[/blue]"
             )
@@ -35,50 +35,73 @@ class ExpressDasIlhasScraper(BaseScraper):
 
             load_more_btn = html.css("#loadMore").get()
             if load_more_btn is not None:
+                # retrieve last_value from the html page and build data argument for POST request
                 pattern = re.compile(r"let last = '([^']*)'")
                 match = pattern.search(resp.text)
                 last_value = match.group(1)
 
                 data = {
-                    'listType': 'section',
-                    'slug': page_url.rsplit('/', 1)[-1],
-                    'before': last_value
+                    "listType": "section",
+                    "slug": page_url.rsplit("/", 1)[-1],
+                    "before": last_value,
                 }
-                endpoint = 'https://expressodasilhas.cv/api/lists/section'
+                endpoint = "api/lists/section"
                 resp = await self.send_post_request(endpoint, data)
-                
-                if resp is not None:
-                    ic(resp)
-                    exit()
-                
-                # await self.parse_page(client, load_more_btn)
+
+                while True:
+                    payload = resp.json()
+
+                    await self.parse_json(payload)
+
+                    # Update the before and slug for argument
+                    data["before"] = payload.get("last")
+                    data["slug"] = payload.get("list")[0].get("slug").split("/")[0]
+
+                    # Send another post request
+                    res = await self.send_post_request(endpoint, data)
+
+                    if res is not None:
+                        await self.parse_json(res.json())
+                    else:
+                        break
             else:
                 console.print(f"No next page found on [blue]{page_url}[/blue]")
 
         except Exception as e:
             console.print(f"[red]Error parsing page {page_url}[/red]: {e}")
 
+    async def parse_json(self, payload):
+        for article_dict in payload.get("list"):
+            async with AsyncClient() as client:
+                link = f"{self.base_url}/{article_dict.get('slug')}"
+                res = await self.fetch_page(client, link)
+                sel = Selector(text=res.text)
+                await self.parse_article(link, sel)
+
     async def parse_article(self, page_url, html):
         try:
             article_block = html.css("div.row.article")
-            author_datepub = article_block.css('.topSignature > p')
-            author = set(author_datepub.css('.intern.author::text').getall())
-            author = ', '.join(map(str, author))
+            author_datepub = article_block.css(".topSignature > p")
+            author = set(author_datepub.css(".intern.author::text").getall())
+            author = ", ".join(map(str, author))
 
             item = {
                 "source": "expressodasilhas",
-                "title": article_block.css('.row > h1::text').get(),
+                "title": article_block.css(".row > h1::text").get(),
                 "author": author,
                 "date_pub": normalize_date(
                     dateparser.parse(
-                        article_block.css('.col-sm-6.topSignature > p > span::text').get(), settings={"TIMEZONE": "UTC-1"}
+                        article_block.css(
+                            ".col-sm-6.topSignature > p > span::text"
+                        ).get(),
+                        settings={"TIMEZONE": "UTC-1"},
                     )
                 ),
                 "link": page_url,
-                "topic": article_block.css('.antetitle > a::text').get(),
+                "topic": article_block.css(".antetitle > a::text").get(),
                 "text_html": hypertext.unescape(
                     " <br/> ".join(
-                        article_block.css('.content > .summary + .articleText').getall()
+                        article_block.css(".content > .summary + .articleText").getall()
                     )
                 ),
             }
@@ -92,13 +115,16 @@ class ExpressDasIlhasScraper(BaseScraper):
 
 async def main():
     start_urls_santiago = [
-        "/economia",
         "/politica",
+        "/economia",
+        "/pais",
+        "/mundo",
         "/cultura",
-        "/diaspora",
-        "/editorial",
-        "/outros-mundos",
-        "/sociedade",
+        "/desporto",
+        "/empresas-negocios",
+        "/eitec",
+        "/lifestyle",
+        "/opiniao",
     ]
 
     # Create an async multiprocessing Queue for inter-process communication

26248dff1d3ebf7d9f1480f2d67348867b8b52ce
Eric Tavares <ericmartial.tavares@gmail.com>
2023-12-07 14:02:47 -0100

    Naming better imported functions.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 # main_script.py
 
 import asyncio
-from .AnacaoScraper import main as anacaoMain
-from .SantiagoMagazineScraper import main as stMain
-from .ExpressDasIlhasScraper import main as expMain
+from .AnacaoScraper import main as AnacaoMain
+from .SantiagoMagazineScraper import main as SantiagoMagazineMain
+from .ExpressDasIlhasScraper import main as ExpressoDasIlhasMain
 
 async def main():
     # Run script1_main and script2_main concurrently
-    await asyncio.gather(anacaoMain(), stMain(), expMain())
+    await asyncio.gather(AnacaoMain(), SantiagoMagazineMain(), ExpressoDasIlhasMain())
 
 if __name__ == "__main__":
     asyncio.run(main())

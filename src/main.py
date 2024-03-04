78fd204cb7c62cfbf832b013dc7ed9f08a15399e
Eric Tavares <ericmartial.tavares@gmail.com>
2024-03-04 11:49:25 -0100

    Updated imports for main.py file.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import asyncio
 
-from .crawlers.anacao_scraper import main as anacao_main
-from .crawlers.expressodasilhas_scraper import main as expressodasilhas_main
-from .crawlers.santiagomagazine_scraper import main as santiagomagazine_main
+from .scrapers.anacao_scraper import main as anacao_main
+from .scrapers.expressodasilhas_scraper import main as expressodasilhas_main
+from .scrapers.santiagomagazine_scraper import main as santiagomagazine_main
 
 
 async def main():
-    await asyncio.gather(expressodasilhas_main(), expressodasilhas_main())
+    await asyncio.gather(anacao_main(), expressodasilhas_main(), santiagomagazine_main())
 
 
 if __name__ == "__main__":
